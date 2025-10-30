e1301ac84d8190df1d95ce2cacf53a99a5827846
johannesfriedl <johannesfriedl21@web.de>
2025-10-30 17:18:45 +0100

    change for task 5

--- main.m
+++ main.m
@@ -1,10 +1,5 @@
-/ This is the main function 
-a = 1;
-b = exp(a);
-fprintf('%f\n', a+b);
-fprintf('%f\n', a-b);
-fprintf('%f\n', a*b);
-fprintf('%f\n', a/b);
-fprintf('%f\n', a^b);
-fprintf('%f\n', a\b);
-fprintf('%f\n', a^b);
+// This is the main function
+a=1;
+b=exp(a);
+fprintf("%f\n", a+b);
+plot(0:2, 0:2);
